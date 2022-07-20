c2f26d18661b9390c5646c4e0bdea74f04797490
Joe Sasson <sassonjoe66@gmail.com>
2022-07-20 16:18:01 -0400

    Update numpy_rnn.py

--- numpy_rnn.py
+++ numpy_rnn.py
@@ -22,14 +22,35 @@ class Vocabulary:
             self.word2count[word] += 1
 
     def _add_sentence(self, sentence):
-       sentence = sentence.lower()
-       new = self._clean_sentence(sentence=sentence)
-       new = new.replace('\n', '')
-       for word in new.split(' '):
-         self._add_word(word)
+        sentence = sentence.lower()
+        new = self._clean_sentence(sentence=sentence)
+        new = new.replace('\n', '')
+        for word in new.split(' '):
+            self._add_word(word)
+            
+        if len(new.split(' ')) > self.length_of_longest_sentence:
+            self.length_of_longest_sentence = len(new.split(' '))
       
-       self.num_sentences += 1
-
+        self.num_sentences += 1
+        
+    def pad_sequences(self, sequence):
+        """
+        Params:
+        sequence --> numpy.array, Integer vector of tokenized words
+        
+        Returns:
+        padded_sequence --> Integer vector of tokenized words with padding
+        """
+        return_arr = []
+        
+        for s in sequence:
+            new = list(s)
+            missing = self.length_of_longest_sentence - len(new)
+            new.extend([0]*missing)
+            return_arr.append(new)
+            
+        return np.vstack(return_arr)
+    
     def compile_vocab(self, corpus):
         """
         Creates vocabulary
