66ce28ffd162bf821f3b5ae2dd799e71dad11712
Joe Sasson <sassonjoe66@gmail.com>
2022-08-18 12:22:24 -0400

    updated docstrings

--- notebooks/embedding.py
+++ notebooks/embedding.py
@@ -7,16 +7,19 @@ class EmbeddingLayer:
         self.hidden_dim = hidden_dim
         self.weights = np.random.randn(vocab_size, hidden_dim) ## (vocab_size, hidden_dim)
 
-    def predict(self, array):
+    def forward(self, array):
         """
         PARAMS:
           array: 
            -- integer matrix of batch_size x seq_length
+           or
+           -- integer matrix of seq_length x 1 for a single batch
 
         RETURNS:
           array:
            -- integer matrix of batch_size x seq_length x hidden_dim
-           -- the word vectors for each word in the tokenized input
+           or
+           -- seq_length x hidden_dim for a single batch
         """
         assert np.max(array) <= self.vocab_size
 
