4ed1935ceed2eda6e9808a20766512025a56c05d
Joe Sasson <sassonjoe66@gmail.com>
2023-01-25 16:41:56 -0500

    building out for multi-layer rnn option

--- layers/rnn.py
+++ layers/rnn.py
@@ -1,8 +1,6 @@
 from typing import Any
 import numpy as np
 
-np.random.seed(99)
-
 class RNN:
     def __init__(self, hidden_size, vocab_size, seq_length):
         self.name = 'RNN'
@@ -79,49 +77,3 @@ class RNN:
         self.running_loss.append(loss)
 
         return loss, hs[len(x)-1]
-
-
-if __name__ == "__main__":
-
-    ## start with data
-    data = open('/Users/joesasson/Desktop/open-source/numpy-RNN/data/input.txt', 'r').read() # should be simple plain text file
-
-    chars = list(set(data))
-    data_size, vocab_size = len(data), len(chars)
-
-    char_to_idx = { ch:i for i,ch in enumerate(chars) }
-    idx_to_char = { i:ch for i,ch in enumerate(chars) }
-
-    ## hyper-params
-    seq_length = 8
-    hidden_size = 100
-
-    rnn = RNN(hidden_size=hidden_size, vocab_size=vocab_size, seq_length=seq_length)
-
-    def train(rnn, epochs, data, lr=1e-1):
-
-        for _ in range(epochs):
-
-            # prepare inputs (we're sweeping from left to right in steps seq_length long)
-            if rnn.pointer+seq_length+1 >= len(data) or rnn.iteration == 0: 
-                hprev = np.zeros((hidden_size,1)) # reset RNN memory
-                p = 0 # go from start of data
-
-            x = [char_to_idx[ch] for ch in data[rnn.pointer:rnn.pointer+seq_length]]
-            y = [char_to_idx[ch] for ch in data[rnn.pointer+1:rnn.pointer+seq_length+1]]
-
-            # forward / backward pass single batch through network
-            loss, hprev = rnn(inputs=x, targets=y, hprev=hprev, lr=lr)
-
-            # update loss
-            rnn.loss = rnn.loss * 0.999 + loss * 0.001
-
-            ## show progress now and then
-            if rnn.iteration % 1000 == 0: 
-                print('iter {}, loss: {}'.format(rnn.iteration, rnn.loss))
-
-            rnn.pointer += seq_length # move data pointer
-            rnn.iteration += 1 # iteration counter 
-
-            
-    train(rnn=rnn, epochs=50000, data=data)
